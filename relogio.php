3c8bf54c30567734d344f0be4d71088a730eb1e0
Mauro <54038705+MauroTi@users.noreply.github.com>
2023-03-31 13:31:54 -0300

    adicionado deslizante muda tom

--- relogio.php
+++ relogio.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html class="container">
 <head>
   <style>
     #logo:hover {
@@ -51,44 +51,84 @@
 
     #clock {
       position: absolute;
-     // bottom: 0;
-     margin-top: 50px;
+      // bottom: 0;
+      margin-top: 50px;
       width: 100%;
       text-align: center;
-     color: blue;
+      color: blue;
     }
 
     #clock input {
-  border: none;
-  font-size: 70px;
-  text-align: center;
-  color:  #4682B4;
+      border: none;
+      font-size: 70px;
+      text-align: center;
+      color:  #4682B4;
+    }
+
+  .container{
+    filter: invert(var(--invert-value, 100%));
+    background-color: #DCDCDC;
+  }
+  #invert-slider {
+  width: 200px;
+  height: 20px;
+  background-color: #ccc;
+  border-radius: 10px;
+  outline: none;
+  margin-left: 10px;
+  margin-right: 10px;
 }
+
+#invert-slider::-webkit-slider-thumb {
+  width: 20px;
+  height: 20px;
+  background-color: #4682B4;
+  border-radius: 50%;
+  -webkit-appearance: none;
+  margin-top: 0px;
+}
+
+#invert-slider::-moz-range-thumb {
+  width: 20px;
+  height: 20px;
+  background-color: #4682B4;
+  border-radius: 50%;
+  appearance: none;
+  margin-top: -8px;
+}
+
 
 
   </style>
   <link href="logo.css" rel="">
 </head>
 <body>
-  <div id="header">
-    <a href="http://www.alfatek.com.br"><img id="logo" src="logo.png" alt="Logo"></a>
-    <label for="datetime" style="display: none;"></label>
+  <div>
+    <div id="header">
+      <a href="http://www.alfatek.com.br"><img id="logo" src="logo.png" alt="Logo"></a>
+      <label for="datetime" style="display: none;"></label>
     </div>
     <div id="clock">     
       <input type="text" name="datetime" id="datetime" value="" readonly style="border: none; font-size: 70px; text-align: center;">
     </div>
-  
-  <script>
-   function updateDateTime() {
-    var date = new Date();
-    var options = {weekday: 'short', year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'};
-    var dateString = date.toLocaleDateString('pt-BR', options);
-    var dateTimeField = document.getElementById("datetime");
-    dateTimeField.value = dateString;
-  }
-  setInterval(updateDateTime, 1000);
 
-</script>
+    <script>
+     function updateDateTime() {
+      var date = new Date();
+      var options = {weekday: 'short', year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'};
+      var dateString = date.toLocaleDateString('pt-BR', options);
+      var dateTimeField = document.getElementById("datetime");
+      dateTimeField.value = dateString;
+    }
+    setInterval(updateDateTime, 1000);
+
+  </script>
+  <div>
+  <label for="invert-slider">Mudança de Tonalidade</label>
+  <input type="range" id="invert-slider" min="0" max="100" value="100" step="1" oninput="document.documentElement.style.setProperty('--invert-value', this.value + '%');">
+</div>
+
+</div>
 </body>
 
 </html>
