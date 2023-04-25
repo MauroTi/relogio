217a8ed1a005eba61843efd088bdbe4041c5a8cc
Mauro <54038705+MauroTi@users.noreply.github.com>
2023-04-25 17:49:59 -0300

    adicionado clima

--- relogio.php
+++ relogio.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html class="container">
 <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
   <style>
     #logo:hover {
       animation: shake 0.5s;
@@ -67,7 +69,7 @@
 
     .container{
       filter: invert(var(--invert-value, 100%));
-      background-color: #DCDCDC;
+      background-color: #FFFFFF;
     }
     #invert-slider {
       width: 200px;
@@ -96,6 +98,16 @@
       appearance: none;
       margin-top: -8px;
     }
+
+    #rodape {
+      position: absolute;
+      bottom: 0;
+      width: 100%;
+      height: 100px;
+      
+    }
+
+
     /*.cancel-alarm {
       background-color: #e74c3c;
       color: white;
@@ -104,6 +116,34 @@
       font-size: 16px;
       cursor: pointer;
     }*/
+    @media screen and (max-width: 768px) {
+      #logo {
+        width: 100%;
+      }
+
+      #clock {
+        font-size: 7vw;
+      }
+
+      #clock input {
+        font-size: 12vw;
+      }
+    }
+
+    @media screen and (max-width: 480px) {
+      #logo {
+        width: 100%;
+      }
+
+      #clock {
+        font-size: 10vw;
+        margin-top: 10%;
+      }
+
+      #clock input {
+        font-size: 18vw;
+      }
+    }
 
 
 
@@ -174,6 +214,16 @@ checkAlarm();
 </div>
 
 </div>
+<body>
+  <!-- conteúdo da página -->
+  <div id="espaco-rodape"></div>
+<!-- weather widget start --><div id="m-booked-prime-33718"> <div class="booked-wzsp-prime-in"> <div class="booked-wzsp-prime-data"> <div class="booked-wzsp-prime-img wt18"></div> <div class="booked-wzsp-day-val"> <div class="booked-wzsp-day-number"><span class="plus">+</span>24</div> <div class="booked-wzsp-day-dergee"> <div class="booked-wzsp-day-dergee-val">&deg;</div> <div class="booked-wzsp-day-dergee-name">C</div> </div> </div> </div> </div> </div> </div><script type="text/javascript"> var css_file=document.createElement("link"); var widgetUrl = location.href; css_file.setAttribute("rel","stylesheet"); css_file.setAttribute("type","text/css"); css_file.setAttribute("href",'https://s.bookcdn.com/css/w/booked-wzs-widget-prime.css?v=0.0.1'); document.getElementsByTagName("head")[0].appendChild(css_file); function setWidgetData_33718(data) { if(typeof(data) != 'undefined' && data.results.length > 0) { for(var i = 0; i < data.results.length; ++i) { var objMainBlock = document.getElementById('m-booked-prime-33718'); if(objMainBlock !== null) { var copyBlock = document.getElementById('m-bookew-weather-copy-'+data.results[i].widget_type); objMainBlock.innerHTML = data.results[i].html_code; if(copyBlock !== null) objMainBlock.appendChild(copyBlock); } } } else { alert('data=undefined||data.results is empty'); } } var widgetSrc = "https://widgets.booked.net/weather/info?action=get_weather_info;ver=7;cityID=43279;type=5;scode=44296;ltid=3458;domid=w209;anc_id=61960;countday=undefined;cmetric=1;wlangID=1;color=137AE9;wwidth=160;header_color=ffffff;text_color=333333;link_color=08488D;border_form=1;footer_color=ffffff;footer_text_color=333333;transparent=0;v=0.0.1";widgetSrc += ';ref=' + widgetUrl;widgetSrc += ';rand_id=33718';var weatherBookedScript = document.createElement("script"); weatherBookedScript.setAttribute("type", "text/javascript"); weatherBookedScript.src = widgetSrc; document.body.appendChild(weatherBookedScript) </script><!-- weather widget end -->
+ 
+  <div id="rodape">
+    
+    <div id="cont_c79162f30b9a216bb346ceac6ebf111c"><script type="text/javascript" async src="https://www.tempo.com/wid_loader/c79162f30b9a216bb346ceac6ebf111c"></script></div>
+  </div>
 </body>
 
+</body>
 </html>
